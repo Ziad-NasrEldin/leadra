8df657b6f1962f577e605ee9be716cf7aa347f94
Ziad Ahmed <112062841+Ziad-NasrEldin@users.noreply.github.com>
2026-05-15 07:56:25 +0300

    chore: restore Agentation dev toolbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Agentation } from 'agentation'
 import './index.css'
 import App from './App.tsx'
 import { LocaleProvider } from './lib/i18n'
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
       <LocaleProvider>
         <QueryClientProvider client={queryClient}>
           <App />
+          {import.meta.env.DEV && <Agentation />}
         </QueryClientProvider>
       </LocaleProvider>
     </ThemeProvider>
